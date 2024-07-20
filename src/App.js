0d4b98092491d99fe64ad2c2076c212f05ceedc7
willfelixd <willfelixd@gmail.com>
2024-07-19 21:34:34 -0300

    Data Fetching

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Logo from "./img/logo.png"
@@ -6,6 +8,19 @@ import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect (() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
       <img src={Logo} alt="Logo" width="80" height="40" title="Logo" />
