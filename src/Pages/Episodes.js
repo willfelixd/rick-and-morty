ceb4607e33c50a77b5ec0bec576a49a6dc299521
willfelixd <willfelixd@gmail.com>
2024-07-29 12:34:00 -0300

    Episodes

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import Cards from "../components/Cards/Cards"
+import InputGroup from "../components/Filters/Category/InputGroup";
+import styles from "../Pages/Pages.module.scss";
 
 const Episodes = () => {
+
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  let [ results, setResults ] = useState([]);
+  let { air_date, name } = info;
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    (async function(){
+      let data = await fetch(api).then(res => res.json());
+      setInfo(data);
+
+      let a = await Promise.all(
+        data.characters.map((x) => {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
+    })();
+  }, [api]);
   return (
-    <div>
-      Episodes
+    <div style={{ backgroundColor: "#181818" }} className="Episodes">
+      <div className="container">
+        <div className="row mb-4">
+          <h1 className={`${styles.nameWhite} text-center mt-4 mb-4`}>
+            Episódio : {" "}
+            <span className={`${styles.name} text-info`}>
+            {name === "" ? "Unknown" : name}
+            </span>
+          </h1>
+          <h5 className={`${styles.nameWhite} text-center`}>Data de estreia : {air_date === "" ? "Unknown" : air_date}</h5>
+        </div>
+        <div className="row">
+          <div className="col-lg-3 col-12 mb-4">
+            <h4 className="text-center mb-4 text-info">Escolha o episódio</h4>
+            <InputGroup setID={setID} name="Episódio" total={51} />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Filters/Category/InputGroup.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const InputGroup = ({ total, name, setID }) => {
+  return (
+    <div className="input-group mb-3">
+        <select 
+          onChange={(e)=>setID(e.target.value)}
+          className="form-select" 
+          id={name}
+        ><option value="1">Escolha...</option>
+          {[...Array(total).keys()].map((x, index) => {
+              return (
+              <option value={x + 1}>
+                {name} - {x + 1}
+              </option>
+            );  
+          })}
+        </select>
+   </div>
+  );
+};
+
+export default InputGroup
